698af51ff8bffc7e1a8352a35b1c10a5c5e70352
Beruto <beruto000@gmail.com>
2017-10-12 20:00:08 +0100

    Algorithms renewed 3.0 and SMS and Morse finished and tested

--- app/src/main/java/com/cit/albertjimenez/asn1converter/algorithm/Converter.kt
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/algorithm/Converter.kt
@@ -1,23 +1,21 @@
 package com.cit.albertjimenez.asn1converter.algorithm
 
-import com.cit.albertjimenez.asn1converter.extension.getPhoneticCode
-import java.lang.StringBuilder
+import com.cit.albertjimenez.asn1converter.extension.toPhoneticCode
+import com.cit.albertjimenez.asn1converter.extension.toShorten
+import com.cit.albertjimenez.asn1converter.extension.toASCII
 
 /**
  * Created by Albert Jiménez on 12/10/17 for Programming Mobile Devices.
  */
 
-
-
 fun textToASCII(text: String): String {
-    val stb: StringBuilder = StringBuilder()
-    text.toCharArray().forEach {
-        stb.append(it.toInt())
-        stb.append(" ")
-    }
-    return stb.toString()
+    return text.toASCII()
 }
 
 fun textToPhonetic(text: String): String {
-    return text.getPhoneticCode()
+    return text.toPhoneticCode()
+}
+
+fun textToSMS(text: String): String {
+    return text.toShorten()
 }
--- app/src/main/java/com/cit/albertjimenez/asn1converter/extension/Extension.kt
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/extension/Extension.kt
@@ -12,8 +12,19 @@ package com.cit.albertjimenez.asn1converter.extension
 private val phoneticList = listOf("alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf",
         "hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec",
         "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "x-ray", "yankee", "zulu")
+private val dictSMS = mapOf("post script" to "P.S.", "as soon as posible" to "A.S.A.P", "you" to "U",
+        "because" to "BC", "see" to "C", "estimated time of arrival" to "E.T.A.", "do it yourself" to "D.I.Y",
+        "to be honest" to "tbh", "thought" to "tho", "road" to "rd", "avenue" to "Ave", "for example" to "e.g.")
 
-fun String.getPhoneticCode(): String {
+private val alphabet = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i",
+        "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+        "v", "w", "x", "y", "z", " ")
+
+private val morse = listOf(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+        ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+        "...-", ".--", "-..-", "-.--", "--..", "|")
+
+fun String.toPhoneticCode(): String {
     var elem = ""
     forEach {
         val myChar = it.toString()
@@ -23,4 +34,32 @@ fun String.getPhoneticCode(): String {
         }
     }
     return elem
+}
+
+fun String.toMorseCode(): String {
+    val stb = StringBuilder()
+    this.toLowerCase().forEach {
+        if (it.toString() == " ") stb.append(" ")
+        val result = if (it.toString() in alphabet) morse[alphabet.indexOf(it.toString())] else "?"
+        stb.append(result)
+    }
+    return stb.toString()
+}
+
+fun String.toShorten(): String {
+    val splittedArray = this.split(" ")
+    val stb = StringBuilder()
+    splittedArray.forEach {
+        val result = if (it.toLowerCase() in dictSMS) dictSMS[it.toLowerCase()] + " " else it + " "
+        stb.append(result)
+    }
+    return stb.toString()
+}
+
+fun String.toASCII(): String {
+    val stb = StringBuilder()
+    this.toCharArray().forEach {
+        stb.append(it.toInt().toString() + " ")
+    }
+    return stb.toString()
 }
