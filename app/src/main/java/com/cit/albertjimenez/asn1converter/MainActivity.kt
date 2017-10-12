94ba0f10f934d866324b1e1690dfcbc5831057ba
Beruto <beruto000@gmail.com>
2017-10-12 13:04:41 +0100

    First commit

---
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cit.albertjimenez.asn1converter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
