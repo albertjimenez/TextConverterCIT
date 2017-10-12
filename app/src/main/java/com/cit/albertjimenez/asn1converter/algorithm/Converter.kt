5df84dc2e7917b03768d090919644351b2b917e3
Beruto <beruto000@gmail.com>
2017-10-12 15:00:45 +0100

    renewed phonetic algorithm

--- app/src/main/java/com/cit/albertjimenez/asn1converter/algorithm/Converter.kt
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/algorithm/Converter.kt
@@ -1,15 +1,13 @@
 package com.cit.albertjimenez.asn1converter.algorithm
 
-import com.cit.albertjimenez.asn1converter.extension.getElemFromInitial
+import com.cit.albertjimenez.asn1converter.extension.getPhoneticCode
 import java.lang.StringBuilder
 
 /**
  * Created by Albert Jiménez on 12/10/17 for Programming Mobile Devices.
  */
 
-private val phoneticList = listOf("alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf",
-        "hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec",
-        "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "x-ray", "yankee", "zulu")
+
 
 fun textToASCII(text: String): String {
     val stb: StringBuilder = StringBuilder()
@@ -21,10 +19,5 @@ fun textToASCII(text: String): String {
 }
 
 fun textToPhonetic(text: String): String {
-    val stb: StringBuilder = StringBuilder()
-    text.toLowerCase().forEach {
-        stb.append(phoneticList.getElemFromInitial(it.toString()))
-        stb.append(" ")
-    }
-    return stb.toString()
+    return text.getPhoneticCode()
 }
--- app/src/main/java/com/cit/albertjimenez/asn1converter/extension/Extension.kt
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/extension/Extension.kt
@@ -9,11 +9,18 @@ package com.cit.albertjimenez.asn1converter.extension
  *
  * @return empty string if there is no result, or the String associated
  */
-fun List<String>.getElemFromInitial(string: String): String {
+private val phoneticList = listOf("alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf",
+        "hotel", "india", "juliet", "kilo", "lima", "mike", "november", "oscar", "papa", "quebec",
+        "romeo", "sierra", "tango", "uniform", "victor", "whiskey", "x-ray", "yankee", "zulu")
+
+fun String.getPhoneticCode(): String {
     var elem = ""
     forEach {
-        if (it.toLowerCase()[0] == string.toLowerCase()[0])
-            elem = it.toLowerCase()
+        val myChar = it.toString()
+        phoneticList.forEach {
+            if (it.toLowerCase()[0] == myChar.toLowerCase()[0])
+                elem += it.toLowerCase() + " "
+        }
     }
     return elem
 }
