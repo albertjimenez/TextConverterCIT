698d05100287035b0facbb588f1fdc75a2e2c06a
Beruto <beruto000@gmail.com>
2017-10-14 14:05:05 +0100

    Translation created. Live conversion implemented

--- app/src/main/java/com/cit/albertjimenez/asn1converter/MainActivity.kt
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/MainActivity.kt
@@ -2,25 +2,72 @@ package com.cit.albertjimenez.asn1converter
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.cit.albertjimenez.asn1converter.algorithm.textToASCII
+import com.cit.albertjimenez.asn1converter.algorithm.textToMorse
+import com.cit.albertjimenez.asn1converter.algorithm.textToPhonetic
+import com.cit.albertjimenez.asn1converter.algorithm.textToSMS
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     private val selectionItems = listOf("Morse", "SMS", "ASCII", "Phonetic")
+    var selection = selectionItems[0]
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//TODO do the spinner
+        spinner.adapter = ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, selectionItems)
+
+
+        first_button.setOnClickListener { toast(getString(R.string.action_firs_button)) }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Log.d("SPINNER: ", "No item selected")
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                selection = selectionItems[p2]
+                if (!first_edittext.text.isNullOrBlank()){
+                    val myText = first_edittext.text.toString()
+                    when (selection) {
+                        "Morse" -> textView.text = textToMorse(myText)
+                        "SMS" -> textView.text = textToSMS(myText)
+                        "ASCII" -> textView.text = textToASCII(myText)
+                        else -> textView.text = textToPhonetic(myText)
+
+                    }
+                }
+            }
+        }
+
+        first_edittext.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val myText = p0!!.toString()
+                when (selection) {
+                    "Morse" -> textView.text = textToMorse(myText)
+                    "SMS" -> textView.text = textToSMS(myText)
+                    "ASCII" -> textView.text = textToASCII(myText)
+                    else -> textView.text = textToPhonetic(myText)
+
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
     }
+
 }
 
-class MySpinnerListener : AdapterView.OnItemSelectedListener {
-    override fun onNothingSelected(p0: AdapterView<*>?) {
 
-    }
-
-    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-
-    }
-}
