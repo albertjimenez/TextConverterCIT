5f0058433a9192ae09be152dc26921d64d3681a5
Beruto <beruto000@gmail.com>
2017-10-17 16:48:53 +0100

    Numbers added on Morse

--- app/src/main/java/com/cit/albertjimenez/asn1converter/extension/Extension.kt
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/extension/Extension.kt
@@ -16,11 +16,12 @@ private val dictSMS by lazyOf(mapOf("post script" to "P.S.", "as soon as posible
 
 private val alphabet by lazyOf(listOf("a", "b", "c", "d", "e", "f", "g", "h", "i",
         "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-        "v", "w", "x", "y", "z", " "))
+        "v", "w", "x", "y", "z", " ","0","1","2","3","4","5","6","7","8","9"))
 
 private val morse by lazyOf(listOf(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
         ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-",
-        "...-", ".--", "-..-", "-.--", "--..", "|"))
+        "...-", ".--", "-..-", "-.--", "--..", "|","----",".----","..---","...--","....-",".....",
+        "-....","--...","---..","----."))
 
 /**
  * Extension method for converting the String itself to a Phonetic Code
