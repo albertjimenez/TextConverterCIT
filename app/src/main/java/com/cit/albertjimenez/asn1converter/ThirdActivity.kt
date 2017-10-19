7a7953c9ff37677e5dec3b9384f270c5952860af
Beruto <beruto000@gmail.com>
2017-10-19 10:19:48 +0100

    animations added 2

--- app/src/main/java/com/cit/albertjimenez/asn1converter/ThirdActivity.kt
+++ app/src/main/java/com/cit/albertjimenez/asn1converter/ThirdActivity.kt
@@ -3,6 +3,7 @@ package com.cit.albertjimenez.asn1converter
 import android.os.Bundle
 import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_third.*
 import org.jetbrains.anko.intentFor
@@ -12,7 +13,6 @@ class ThirdActivity : AppCompatActivity() {
 
     private val imageURL = "http://lorempixel.com/400/200/"
 
-    //TODO animations
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +20,8 @@ class ThirdActivity : AppCompatActivity() {
         goback.setOnClickListener { startActivity(intentFor<MainActivity>("DATE"
                 to GregorianCalendar.getInstance().time.toString())) }
         Glide.with(this).load(imageURL).into(imageViewNet)
-                .onLoadStarted(ResourcesCompat.getDrawable(resources, R.drawable.no_internet, null)
-        )
+                .onLoadStarted(ResourcesCompat.getDrawable(resources, R.drawable.no_internet, null))
+        imageViewNet.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_up))
+
     }
 }
